e1bcbe72928dc6d4b278cf5a63b3794b9e779379
yongseondu <weed0124@naver.com>
2020-07-30 07:58:58 +0900

    Initial commit

---
+++ src/main/java/jpabook/yondu_jpashop/YonduJpashopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.yondu_jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonduJpashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonduJpashopApplication.class, args);
+	}
+
+}
