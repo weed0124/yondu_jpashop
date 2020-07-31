dec80a75573dba1678f165fd322e37b3378012e6
yongseondu <weed0124@naver.com>
2020-07-31 09:31:19 +0900

    즉시로딩(EAGER) -> 지연로딩(LAZY)으로 변경 EAGER는 예측이 어렵고 어떤 SQL이 실행될지 추적하기 어렵고 JPQL을 실행할 때 N+!문제가 자주 발생하기 때문

--- src/main/java/jpabook/yondu_jpashop/domain/Order.java
+++ src/main/java/jpabook/yondu_jpashop/domain/Order.java
@@ -7,6 +7,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -15,14 +17,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
