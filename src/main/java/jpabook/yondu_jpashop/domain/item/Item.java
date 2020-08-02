96ff5e9ab78493eed260f0625d11fcf2f490a378
yongseondu <weed0124@naver.com>
2020-08-03 07:09:59 +0900

    비즈니스 로직 추가(재고 증가/감소)

--- src/main/java/jpabook/yondu_jpashop/domain/item/Item.java
+++ src/main/java/jpabook/yondu_jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.yondu_jpashop.domain.item;
 
 import jpabook.yondu_jpashop.domain.Category;
+import jpabook.yondu_jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity -= quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
