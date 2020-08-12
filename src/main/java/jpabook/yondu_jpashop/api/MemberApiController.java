2fbac8562d30300c71014a88ab0e68462fbe491f
yongseondu <weed0124@naver.com>
2020-08-13 07:11:12 +0900

    회원조회 API

--- src/main/java/jpabook/yondu_jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/yondu_jpashop/api/MemberApiController.java
@@ -9,11 +9,49 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    /**
+     * 엔티티 정보가 외부로 노출됨
+     * 엔티티 변경시 API 스펙이 변경됨
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    /**
+     * 외부에 노출시킬 전용 DTO를 만들어 엔티티를 한번 감싸서 보내주어야함
+     * 추후 API 기능 추가 시 엔티티 변경 없이 DTO만 변경하면되어 유지보수성이 좋아짐
+     */
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     /**
      * 화면단에서도 사용하는 엔티티를 그대로 사용시 추후 문제가 발생하므로 별도의 API전용 DTO가 필요함
