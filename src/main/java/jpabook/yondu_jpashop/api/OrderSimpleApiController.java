fcefd603a02924c918e01ed5830c45247d4aefc9
yongseondu <weed0124@naver.com>
2020-08-18 08:11:51 +0900

    Version1: 엔티티를 직접 노출

---
+++ src/main/java/jpabook/yondu_jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,33 @@
+package jpabook.yondu_jpashop.api;
+
+import jpabook.yondu_jpashop.domain.Order;
+import jpabook.yondu_jpashop.repository.OrderRepository;
+import jpabook.yondu_jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+        return all;
+    }
+}
