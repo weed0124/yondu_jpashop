fd1522aba3852c9daf5d510806c12361a7404186
yongseondu <weed0124@naver.com>
2020-08-18 08:11:28 +0900

    Hibernate5Module 추가

--- src/main/java/jpabook/yondu_jpashop/YonduJpashopApplication.java
+++ src/main/java/jpabook/yondu_jpashop/YonduJpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.yondu_jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class YonduJpashopApplication {
@@ -10,4 +12,10 @@ public class YonduJpashopApplication {
 		SpringApplication.run(YonduJpashopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
+	}
 }
