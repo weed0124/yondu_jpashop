6429ca66aa0d71cb4509896dcdefd9bdb71d9a11
yongseondu <weed0124@naver.com>
2020-09-04 06:49:04 +0900

    SpringData JPA 적용

--- src/main/java/jpabook/yondu_jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/yondu_jpashop/repository/MemberRepository.java
@@ -1,35 +1,13 @@
 package jpabook.yondu_jpashop.repository;
 
 import jpabook.yondu_jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
-    private final EntityManager em;
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
+    // select m from Member m where m.name = ?
+    // findByXXXX => XXXX에 해당되는 조건 쿼리가 자동으로 생성됨
+    List<Member> findByName(String name);
 }
